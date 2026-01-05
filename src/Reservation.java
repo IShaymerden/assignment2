ca6b8b1e0ea677e7f5e2196cdfaa52e44b262bb4
Iskander <bebrakebaba@gmail.com>
2026-01-05 23:30:32 +0500

    first-commit

--- src/Reservation.java
+++ src/Reservation.java
@@ -11,10 +11,34 @@ public class Reservation {
         this.seatNumber = seatNumber;
     }
 
+    // ЭТОТ МЕТОД НУЖЕН, ЧТОБЫ ОШИБКА В MAIN ИСЧЕЗЛА
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
     @Override
     public String toString() {
-        return "Reservation Details:\n" + passenger + "\n" + flight + "\nSeat: " + seatNumber;
+        return "Reservation:\n" + passenger + "\n" + flight + "\nSeat: " + seatNumber;
     }
 }
-
 
